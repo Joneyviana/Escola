4b8645b6425b5f63fd6136a6ab50a16d35f44ad4
Joneyviana <arcistiel@gmail.com>
2016-02-19 08:40:49 -0200

    melhorando a questão da chave estrangeira

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,19 +1,21 @@
+
 class ClassroomsController < ApplicationController
 
   def index
-     
+
   end
 
   def new
     @classroom =  Classroom.new
-    @student = Student.find(params[:id])
+    @student = Student.find(params[:student_id])
     @courses = Course.all()
   end
   def create
-   @classroom = Classroom.new(classroom_params)
-
+   @courses = Course.all()
+   @student = Student.find(params[:student_id])
+   @classroom = @student.classrooms.build(classroom_params)
    if @classroom.save
-     redirect_to Student.find(classroom_params[:student_id])
+     redirect_to @student
    else
      render 'new'
    end
@@ -21,6 +23,6 @@ class ClassroomsController < ApplicationController
 
   private
     def classroom_params
-      params.require(:classroom).permit(:entry_at , :course_id , :student_id)
+      params.require(:classroom).permit(:entry_at , :course_id)
     end
 end
