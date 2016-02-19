e36ea8518e7c43fedb3d0acb38603e418a15522c
Joneyviana <arcistiel@gmail.com>
2016-02-19 08:36:54 -0200

    fazendo validações

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,4 +1,6 @@
 class Classroom < ActiveRecord::Base
   belongs_to :student
   belongs_to :course
+  accepts_nested_attributes_for :student
+  validates_uniqueness_of :student_id , :scope => :course_id
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,8 @@
 class Student < ActiveRecord::Base
-    has_many :classrooms
+    attachment :image
+    validates :name ,presence: true
+    validates :register_number , uniqueness: true , numericality: true
+    validates :status , numericality: true
+    has_many :classrooms , dependent: :destroy
     has_many :courses, through: :classrooms
 end
