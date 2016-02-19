f0db750ab24b3512f2a6aee6c7de22b40eff7606
Joneyviana <arcistiel@gmail.com>
2016-02-19 08:44:07 -0200

    adicionando refile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204000146) do
+ActiveRecord::Schema.define(version: 20160215115413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160204000146) do
     t.integer  "status"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "image_id"
   end
 
   add_foreign_key "classrooms", "courses"
